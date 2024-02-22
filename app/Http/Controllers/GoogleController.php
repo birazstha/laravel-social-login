bff70b9bfd6255c08a9337a580b4462b0b126b58
biraj1234 <biraj.spell@gmail.com>
2024-02-23 00:05:55 +0545

    feat: user can login with their google account

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    protected $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function loginWithGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callBackFromGoogle()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+
+            $authUser = User::where('email', $user->email)->first();
+            if ($authUser) {
+                Auth::login($authUser);
+                return redirect()->intended('/home');
+            }
+
+            // Or you can create a new user
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_avatar' => $user->avatar,
+                'password' => Hash::make($user->name . '@' . $user->getId()),
+            ]);
+
+            Auth::login($newUser);
+            return redirect()->intended('/home');
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\GoogleController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('auth/google', [GoogleController::class, 'loginWithGoogle'])->name('google-login');
+Route::any('auth/google/callback', [GoogleController::class, 'callBackFromGoogle'])->name('google-callback');
